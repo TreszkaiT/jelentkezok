67dc93dac7274bee1b747b1aaf239b0eba7777fc
TreszkaiT <treszkaitom@gmail.com>
2022-08-27 20:21:07 +0200

    Login View Dark 3

--- src/main/java/com/example/app/views/LoginView.java
+++ src/main/java/com/example/app/views/LoginView.java
@@ -52,6 +52,8 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
         loginOverlay.getElement().getThemeList().add("dark");
         LoginI18n i18n = LoginI18n.createDefault();
 
+
+
         i18n.setAdditionalInformation("Ha nem tudnál belépni az adataiddal, akkor kérlek vedd fel a kapcsolatot velünk: admin@company.com");
 
 
@@ -60,7 +62,26 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
         loginOverlay.setDescription("Készítette: T.Tamás");
         loginOverlay.setOpened(true);
         loginOverlay.setError(true);
+
+        /*LoginI18n.Header i18nHeader = i18n.getHeader();
+        i18nHeader.setTitle("Önéletrajz adatok bevitele");
+        i18nHeader.setDescription("Készítette: T.Tamás");
+
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setTitle("Bejelentkezés");
+        i18nForm.setUsername("Felhasználónév");
+        i18nForm.setPassword("Jelszó");
+        i18nForm.setSubmit("Belépés");
+        i18nForm.setForgotPassword("Elfelejtett jelszó");
+        i18n.setForm(i18nForm);
+
+        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
+        i18nErrorMessage.setTitle("Nem jó a felhasználónév vagy a jelszó");
+        i18nErrorMessage.setMessage("Ellenőrizd, hogy a megfelelő felhasználónevet és jelszót adtad-e meg, majd próbáld újra.");
+        i18n.setErrorMessage(i18nErrorMessage);*/
         loginOverlay.setI18n(i18n);
+
+        //loginOverlay.setI18n(i18n);
         loginOverlay.setAction("login");
         loginOverlay.getElement().setAttribute("no-autofocus", "");
         loginOverlay.getElement().getThemeList().add("dark");
@@ -71,12 +92,12 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
         //login.getElement().setAttribute("no-autofocus", "");
 
 
+
         add(
                 //new H1("Önéletrajz adatok bevitele"),
                 loginOverlay
                 //login
         );
-
 
 
     }
