86bd079fd298a6fe8929a26fe65bd5c6a7f48de1
TreszkaiT <treszkaitom@gmail.com>
2022-08-28 10:53:21 +0200

    version kijelzése

--- src/main/java/com/example/app/views/LoginView.java
+++ src/main/java/com/example/app/views/LoginView.java
@@ -1,12 +1,10 @@
 package com.example.app.views;
 
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.BeforeEnterListener;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
