006f914a1c51084a22ce858a5b9a5589bdc8875d
TreszkaiT <treszkaitom@gmail.com>
2022-08-28 08:35:38 +0200

    login magyar

--- src/main/java/com/example/app/views/LoginView.java
+++ src/main/java/com/example/app/views/LoginView.java
@@ -24,11 +24,51 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        /*LoginI18n i18n = LoginI18n.createDefault();
+        loginOverlay.setOpened(true);
+        loginOverlay.getElement().setAttribute("no-autofocus", "");
+        loginOverlay.getElement().getThemeList().add("dark");
 
-        LoginI18n.Header i18nHeader = i18n.getHeader();
-        i18nHeader.setTitle("Önéletrajz adatok bevitele");
-        i18nHeader.setDescription("Készítette: T.Tamás");
+        loginOverlay.setI18n(createLoginI18n());
+
+
+        loginOverlay.setTitle("Önéletrajz adatok bevitele");
+        loginOverlay.setDescription("Készítette: T.Tamás");
+        loginOverlay.setOpened(true);
+        loginOverlay.setError(true);
+
+        loginOverlay.setI18n(createLoginI18n());
+
+        //loginOverlay.setI18n(i18n);
+        loginOverlay.setAction("login");
+        loginOverlay.getElement().setAttribute("no-autofocus", "");
+        loginOverlay.getElement().getThemeList().add("dark");
+
+
+
+
+        add(
+                //new H1("Önéletrajz adatok bevitele"),
+                loginOverlay
+                //login
+        );
+
+        this.getElement().getStyle().set("background-image", "images/art_back.jpg");
+
+    }
+
+    private LoginI18n createLoginI18n(){
+        LoginI18n i18n = LoginI18n.createDefault();
+
+	/*  not sure if needed
+	i18n.setHeader(new LoginI18n.Header());
+	i18n.setForm(new LoginI18n.Form());
+	i18n.setErrorMessage(new LoginI18n.ErrorMessage());
+	*/
+        //LoginI18n i18n = LoginI18n.createDefault();
+
+        //LoginI18n.Header i18nHeader = i18n.getHeader();
+        //i18nHeader.setTitle("Önéletrajz adatok bevitele");
+        //i18nHeader.setDescription("Készítette: T.Tamás");
 
         LoginI18n.Form i18nForm = i18n.getForm();
         i18nForm.setTitle("Bejelentkezés");
@@ -44,62 +84,25 @@ public class LoginView extends VerticalLayout implements BeforeEnterListener {
         i18n.setErrorMessage(i18nErrorMessage);
 
         i18n.setAdditionalInformation("Ha nem tudnál belépni az adataiddal, akkor kérlek vedd fel a kapcsolatot velünk: admin@company.com");
+    /*
+        // define all visible Strings to the values you want
+        // this code is copied from above-linked example codes for Login
+        // in a truly international application you would use i.e. `getTranslation(USERNAME)` instead of hardcoded string values. Make use of your I18nProvider
+        //i18n.getHeader().setTitle("Nome do aplicativo");
+        //i18n.getHeader().setDescription("Descrição do aplicativo");
+        i18n.getForm().setUsername("Usuário"); // this is the one you asked for.
+        i18n.getForm().setTitle("Acesse a sua conta");
+        i18n.getForm().setSubmit("Entrar");
+        i18n.getForm().setPassword("Senha");
+        i18n.getForm().setForgotPassword("Esqueci minha senha");
+        i18n.getErrorMessage().setTitle("Usuário/senha inválidos");
+        i18n.getErrorMessage()
+                .setMessage("Confira seu usuário e senha e tente novamente.");
+        i18n.setAdditionalInformation(
+                "Caso necessite apresentar alguma informação extra para o usuário"
+                        + " (como credenciais padrão), este é o lugar.");
         */
-
-        //loginOverlay.setI18n(i18n);
-        loginOverlay.setOpened(true);
-        loginOverlay.getElement().setAttribute("no-autofocus", "");
-        loginOverlay.getElement().getThemeList().add("dark");
-        LoginI18n i18n = LoginI18n.createDefault();
-
-
-
-        i18n.setAdditionalInformation("Ha nem tudnál belépni az adataiddal, akkor kérlek vedd fel a kapcsolatot velünk: admin@company.com");
-
-
-
-        loginOverlay.setTitle("Önéletrajz adatok bevitele");
-        loginOverlay.setDescription("Készítette: T.Tamás");
-        loginOverlay.setOpened(true);
-        loginOverlay.setError(true);
-
-        /*LoginI18n.Header i18nHeader = i18n.getHeader();
-        i18nHeader.setTitle("Önéletrajz adatok bevitele");
-        i18nHeader.setDescription("Készítette: T.Tamás");
-
-        LoginI18n.Form i18nForm = i18n.getForm();
-        i18nForm.setTitle("Bejelentkezés");
-        i18nForm.setUsername("Felhasználónév");
-        i18nForm.setPassword("Jelszó");
-        i18nForm.setSubmit("Belépés");
-        i18nForm.setForgotPassword("Elfelejtett jelszó");
-        i18n.setForm(i18nForm);
-
-        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
-        i18nErrorMessage.setTitle("Nem jó a felhasználónév vagy a jelszó");
-        i18nErrorMessage.setMessage("Ellenőrizd, hogy a megfelelő felhasználónevet és jelszót adtad-e meg, majd próbáld újra.");
-        i18n.setErrorMessage(i18nErrorMessage);*/
-        loginOverlay.setI18n(i18n);
-
-        //loginOverlay.setI18n(i18n);
-        loginOverlay.setAction("login");
-        loginOverlay.getElement().setAttribute("no-autofocus", "");
-        loginOverlay.getElement().getThemeList().add("dark");
-
-        //login.setAction("login");
-
-        //login.getElement().getThemeList().add("dark");
-        //login.getElement().setAttribute("no-autofocus", "");
-
-
-
-        add(
-                //new H1("Önéletrajz adatok bevitele"),
-                loginOverlay
-                //login
-        );
-
-
+        return i18n;
     }
 
     /**
