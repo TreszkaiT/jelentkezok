e2b5229cfb327edf6d51363a0d8b4d7517e741be
TreszkaiT <treszkaitom@gmail.com>
2022-09-11 22:31:27 +0200

    test 2

--- src/test/java/com/example/app/views/list/form/PersonFormTest.java
+++ src/test/java/com/example/app/views/list/form/PersonFormTest.java
@@ -6,11 +6,14 @@ import com.example.app.data.entity.Person;
 import com.example.app.data.entity.ProfExperience;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PersonFormTest {
     private List<City> cities;
@@ -29,18 +32,18 @@ public class PersonFormTest {
     public void setupData() {
         cities = new ArrayList<>();
         city1 = new City();
-        city1.setName("Vaadin Ltd");
+        city1.setName("Budapest");
         city2 = new City();
-        city2.setName("IT Mill");
+        city2.setName("Nyíregyháza");
         cities.add(city1);
         cities.add(city2);
 
 
         languages = new HashSet<>();
         language1 = new Language();
-        language1.setName("Status 1");
+        language1.setName("Alban");
         language2 = new Language();
-        language2.setName("Status 2");
+        language2.setName("English");
         languages.add(language1);
         languages.add(language2);
 
@@ -61,6 +64,7 @@ public class PersonFormTest {
         person.setProfExperiences(profExperiences);
     }
 
+    @Test
     public void formFieldsPopulated(){
         PersonForm form = new PersonForm(cities, languagesList);
         form.setPerson(person);
@@ -68,6 +72,40 @@ public class PersonFormTest {
         Assert.assertEquals("Sanyi", form.firstName.getValue());
         Assert.assertEquals("Kiss", form.lastName.getValue());
         Assert.assertEquals("kiss@sanyi.com", form.email.getValue());
+        Assert.assertEquals(city2, form.city.getValue());
+        Assert.assertEquals(language1, form.language.getValue());
+
+    }
+
+    @Test
+    public void saveEventHasCorrectValues(){
+        PersonForm form = new PersonForm(cities, languagesList);
+        Person person = new Person();
+        form.setPerson(person);
+
+        form.firstName.setValue("Sanyi");
+        form.lastName.setValue("Kiss");
+        form.email.setValue("kiss@sanyi.com");
+        form.city.setValue(city2);
+        form.language.setValue(languages);
+        form.bornDate.setValue(LocalDate.of(2022,9,11));
+        form.phone.setValue("06-30-3457-852");
+        form.address.setValue("Vass utca 5");
+        form.socialMedia.setValue("facebook");
+        form.messageApps.setValue("messenger");
+        form.webSite.setValue("www.sg.hu");
+        form.otherSkill.setValue("scrum master");
+        form.coverLetter.setValue("Ez a levelem");
+        form.picture.setValue("jo21.png");
+
+        AtomicReference<Person> savePerson = new AtomicReference<>(null);
+        form.addListener(PersonForm.SaveEvent.class, e -> savePerson.set(e.getPerson()));
+
+        form.save.click();
+
+        Person saved = savePerson.get();
+
+        Assert.assertEquals("Sanyi", saved.getfirstName());
 
     }
 }
